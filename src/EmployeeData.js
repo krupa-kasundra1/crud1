f0cca6435bf9e772b3810c8a6a7eccb9700c155b
krupa-kasundra1 <krupakasundra1@gmail.com>
2024-04-11 11:19:56 +0530

    first commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+    {
+        id : 1,
+        firstName :'Mark',
+        lastName : 'David',
+        age : 20
+    },
+    {
+        id : 2,
+        firstName :'Virat',
+        lastName : 'Kohali',
+        age : 34
+    },
+    {
+        id : 3,
+        firstName :'Rohit',
+        lastName : 'Sharma',
+        age : 36
+    },    
+]
